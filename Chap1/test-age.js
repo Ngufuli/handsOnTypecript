2222d4da85d08943092373a7f34697c261f4c880
Ngufuli <ngufuli@outlook.com>
2023-01-16 23:22:23 +0530

    transpiled code

---
+++ Chap1/test-age.js
@@ -0,0 +1,14 @@
+function canDrive(usr) {
+    console.log("User is ", usr.name);
+    if (usr.age >= 16) {
+        console.log("Allow to drive");
+    }
+    else {
+        console.log("Do not allow to drive");
+    }
+}
+var tom = {
+    name: "Tom",
+    age: 20
+};
+canDrive(tom);
