13efdf037fb65f0ebd58d6f56a4f5541be68b933
Ngufuli <ngufuli@outlook.com>
2023-01-24 12:05:24 +0530

    transpiled

--- Chap2/classes.js
+++ Chap2/classes.js
@@ -1,11 +1,12 @@
 var Person = /** @class */ (function () {
-    function Person() {
+    function Person(msg) {
+        this.msg = msg;
     }
     Person.prototype.speak = function () {
         console.log(this.msg);
     };
     return Person;
 }());
-var Nick = new Person();
-Nick.msg = "Hellow world";
+var Nick = new Person("Hello world");
+// Nick.msg = "Hellow world";
 Nick.speak();
