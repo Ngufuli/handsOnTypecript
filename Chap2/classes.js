8e65059f6bfc4051f104252bcd4e703b95d44883
Ngufuli <ngufuli@outlook.com>
2023-01-30 23:02:30 +0530

    minor updates

--- Chap2/classes.js
+++ Chap2/classes.js
@@ -1,12 +1,22 @@
+// class Person{
+//     constructor(private msg: string) {}
+//     speak(){
+//         console.log(this.msg);
+//     }
+// }
+// const Nick = new Person("Hello world");
+// // Nick.msg = "Hellow world";
+// Nick.speak();
+// As readonly
 var Person = /** @class */ (function () {
     function Person(msg) {
         this.msg = msg;
     }
     Person.prototype.speak = function () {
+        this.msg = "speak" + this.msg;
         console.log(this.msg);
     };
     return Person;
 }());
-var Nick = new Person("Hello world");
-// Nick.msg = "Hellow world";
+var Nick = new Person("New tech!");
 Nick.speak();
--- Chap2/classes.ts
+++ Chap2/classes.ts
@@ -1,11 +1,24 @@
-class Person{
-    constructor(private msg: string) {}
+// // class Person{
+// //     constructor(private msg: string) {}
     
-    speak(){
-        console.log(this.msg);
-    }
-}
+// //     speak(){
+// //         console.log(this.msg);
+// //     }
+// // }
 
-const Nick = new Person("Hello world");
-// Nick.msg = "Hellow world";
-Nick.speak();
+// // const Nick = new Person("Hello world");
+// // // Nick.msg = "Hellow world";
+// // Nick.speak();
+
+// // As readonly
+// class Person{
+//     constructor (private readonly msg: string){}
+
+//     speak(){
+//         // this.msg = "speak" + this.msg;
+//         console.log(this.msg);
+//     }
+
+// }
+// const Nick = new Person("New tech!")
+// Nick.speak()
