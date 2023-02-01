9e2158db225ec4b89902362dd57b3f49f0b03704
Ngufuli <ngufuli@outlook.com>
2023-02-01 17:23:49 +0530

    protected data members can be modified by inheritng classes

--- Chap2/classInheritence.ts
+++ Chap2/classInheritence.ts
@@ -1,5 +1,5 @@
 class Vehicle{
-    constructor (private wheelCount: number){
+    constructor (protected wheelCount: number){
 
     }
     showNumberOfWheels(){
@@ -10,6 +10,9 @@ class Vehicle{
 class Motorcycle extends Vehicle{
     constructor(){
         super(2);
+    }
+    updateWheelCount(newWheelCount: number){
+        this.wheelCount = newWheelCount;
     }
 }
 
