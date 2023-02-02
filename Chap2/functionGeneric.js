285ee68a2d8c25cc5b6ef37e71d990a09672ba82
Ngufuli <ngufuli@outlook.com>
2023-02-03 03:03:21 +0530

    update

--- Chap2/functionGeneric.js
+++ Chap2/functionGeneric.js
@@ -1,8 +1,13 @@
+// function getLength<T> (arg: T): number{
+//     if(arg.hasOwnProperty("length")){
+//         return arg["length"]
+//     }
+//     return 0;
+// }
+// console.log(getLength<number>(22));
+// console.log(getLength("Hello Universe!"));
 function getLength(arg) {
-    if (arg.hasOwnProperty("length")) {
-        return arg["length"];
-    }
-    return 0;
+    return arg.length;
 }
 console.log(getLength(22));
-console.log(getLength("Hello Universe!"));
+console.log(getLength("Hello Universe"));
