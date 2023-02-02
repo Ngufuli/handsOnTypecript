5b67cf0d046307f4e4f4281de6247e22b75b425e
Ngufuli <ngufuli@outlook.com>
2023-02-02 23:09:04 +0530

    getset

---
+++ Chap2/getSet.js
@@ -0,0 +1,18 @@
+class Speaker {
+    constructor(name) {
+        this.name = name;
+    }
+    get Message() {
+        if (!this.message.includes(this.name)) {
+            throw Error("message is missing speaker's name");
+        }
+        return this.message;
+    }
+    set Message(val) {
+        let tmpMessage = val;
+        if (!val.includes(this.name)) {
+            tmpMessage = this.name + " " + val;
+        }
+        this.message = tmpMessage;
+    }
+}
