68f1ebedc76555f225ac8906638f0191d9d3a8ae
Ngufuli <ngufuli@outlook.com>
2023-02-03 03:02:40 +0530

    generic function and inteface

--- Chap2/interfaceInheritance.ts
+++ Chap2/interfaceInheritance.ts
@@ -9,4 +9,25 @@ namespace InterfaceNamespace{
         updateWheelCount: (newWheelCount: number) => void;
         showNumberOfWheels: () => void;
     }
+   
+}
+class Motorcycle implements Vehicle{
+    name: string;
+    wheelCount: number;
+    constructor(name: string){
+        //No super in interface
+        this.name = name;
+    }
+    updateWheelCount: (newWheelCount: number){
+        this.wheelCount. newWheelCount;
+        console.log(`Automobile has ${this.wheelCount}`);
+    }
+    showNumberOfWheels(){
+        console.log(`moved Automobile ${this.wheelCount}`)
+    }
+    getFullName(){
+        return "MC-" + this.Name;
+    }
+    const moto = new Motorcycle("beginner-cycle");
+    console.log(moto.getFullName());
 }
