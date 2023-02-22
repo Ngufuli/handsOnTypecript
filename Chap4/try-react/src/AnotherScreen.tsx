7a7048cb6c0f552f5493444a89cb7d7d2a71f2ce
Ngufuli <ngufuli@outlook.com>
2023-02-23 03:37:32 +0530

    another screen components

--- Chap4/try-react/src/AnotherScreen.tsx
+++ Chap4/try-react/src/AnotherScreen.tsx
@@ -1,2 +1,8 @@
-import React from 'react';
+import React,{FC} from 'react';
 // import ReactDOM from 'react-dom/client';
+
+const AnotherScreen: FC = () => {
+    return <div>Hello World! Another Screen</div>
+}
+
+export default AnotherScreen;
