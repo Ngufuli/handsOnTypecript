6efa2545e52e576a7ec91e796bd52339b3edf2fd
Ngufuli <ngufuli@outlook.com>
2023-03-18 22:34:38 +0530

    .

--- chap6/OldStyleWebApp/script.js
+++ chap6/OldStyleWebApp/script.js
@@ -1,2 +1,7 @@
 const inputEl = document.querySelector("#userName");
 console.log("input", inputEl);
+const parE1 = document.querySelector("welcomeMsg");
+
+inputEl.addEventListener("change", (e)=>{
+    parE1.innerHTML = "Welcome " + e.target.value;
+});
