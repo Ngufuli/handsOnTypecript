8c782963e7e2501a5485c456962e2f216b8c1d86
Ngufuli <ngufuli@outlook.com>
2024-10-17 10:31:48 +0530

    .

---
+++ Chap2/runMore.js
@@ -0,0 +1,13 @@
+// function runMore(distance: number): number {
+//     return distance + 10;
+// }
+function priceEarning(priceTo) {
+    console.log("CRBB PLC P/E ratio is " + priceTo);
+}
+function trading(price) {
+    console.log("CRDB is trading at " + price);
+}
+var PE = priceEarning(9);
+console.log(PE);
+var T = trading(640);
+console.log(T);
---
+++ Chap2/runMore.ts
@@ -0,0 +1,17 @@
+// function runMore(distance: number): number {
+//     return distance + 10;
+// }
+
+function priceEarning(priceTo: number){
+    console.log("CRBB PLC P/E ratio is " + priceTo)
+}
+
+function trading(price: number): void{
+    console.log("CRDB is trading at " + price)
+}
+
+let PE = priceEarning(9)
+console.log(PE)
+
+let T = trading(640)
+console.log(T)
