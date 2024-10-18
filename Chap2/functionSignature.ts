8857c7c4ed237813c06109bd0fea3c7c985374f2
Ngufuli <ngufuli@outlook.com>
2024-10-19 03:37:53 +0530

    signature

--- Chap2/functionSignature.ts
+++ Chap2/functionSignature.ts
@@ -1,9 +1,9 @@
 type Run = (miles: number) => boolean;
-let runner: Run = function (miles: number): boolean {
+let runner:Run = function (miles: number): boolean {
     if(miles > 10){
         return true
     }
-    return false;
+    return false
 }
 
-console.log(runner(9));
+console.log(runner(9))
